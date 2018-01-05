f747cede719ee50003fef3261aec98138e280abb
Varun Sundar <varun19299@gmail.com>
2018-01-05 15:05:28 +0530

    Shaastra WS 2

--- windows/conda_install.sh
+++ windows/conda_install.sh
@@ -4,5 +4,8 @@ bash Anaconda3-5.0.1-Linux-x86_64.sh
 conda update conda
 conda update anaconda
 
+conda create -n Shaastra_WS python=3.5
+source activate Shaastra_WS
+
 echo "Installing opencv"
 conda install -c menpo opencv
